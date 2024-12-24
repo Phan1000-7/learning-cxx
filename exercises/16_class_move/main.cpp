9342fed37cd92533d2d9c01b34ff127a22cd8750
Phan1000-7 <691960569@qq.com>
2024-12-24 21:58:20 +0800

    12-24

--- exercises/16_class_move/main.cpp
+++ exercises/16_class_move/main.cpp
@@ -15,7 +15,7 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]{0,1}), cached(2) {}
 
     // TODO: 实现移动构造器
     DynFibonacci(DynFibonacci &&) noexcept = delete;
@@ -25,11 +25,11 @@ public:
     DynFibonacci &operator=(DynFibonacci &&) noexcept = delete;
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci(){delete[] cache};
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t operator[](int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
